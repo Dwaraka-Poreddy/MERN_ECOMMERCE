150b48eeee42889933f1f380a45bddce5435bb8f
Dwaraka-Poreddy <dwarakaporeddy124@gmail.com>
2022-04-26 14:38:10 +0530

    Thirteen sections done, Star Rating System done, showing related products in product page done

--- routes/product.js
+++ routes/product.js
@@ -14,7 +14,10 @@ const {
   update,
   list,
   productsCount,
+  productStar,
+  listRelated,
 } = require("../controllers/product");
+const product = require("../models/product");
 
 // routes
 router.post("/product", authCheck, adminCheck, create);
@@ -24,5 +27,8 @@ router.delete("/products/:slug", authCheck, adminCheck, remove);
 router.get("/product/:slug", read);
 router.put("/product/:slug", authCheck, adminCheck, update);
 router.post("/products", list);
+
+router.put("/product/star/:productId", authCheck, productStar);
+router.get("/product/related/:productId", listRelated);
 
 module.exports = router;
