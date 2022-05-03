8343b0597722f8834f2066af67590bbe27f03ac0
Dwaraka-Poreddy <dwarakaporeddy124@gmail.com>
2022-05-03 16:34:36 +0530

    sections 18 & 19 done. payment considering Coupons is done using Strip

--- controllers/user.js
+++ controllers/user.js
@@ -1,6 +1,7 @@
 const User = require("../models/user");
 const Product = require("../models/product");
 const Cart = require("../models/cart");
+const Coupon = require("../models/coupon");
 
 exports.userCart = async (req, res) => {
   // console.log(req.body); // {cart: []}
@@ -59,8 +60,10 @@ exports.getUserCart = async (req, res) => {
     .populate("products.product", "_id title price totalAfterDiscount")
     .exec();
 
-  const { products, cartTotal, totalAfterDiscount } = cart;
-  res.json({ products, cartTotal, totalAfterDiscount });
+  if (cart) {
+    const { products, cartTotal, totalAfterDiscount } = cart;
+    res.json({ products, cartTotal, totalAfterDiscount });
+  }
 };
 
 exports.emptyCart = async (req, res) => {
@@ -78,4 +81,41 @@ exports.saveAddress = async (req, res) => {
   ).exec();
 
   res.json({ ok: true });
+};
+
+exports.applyCouponToUserCart = async (req, res) => {
+  const { coupon } = req.body;
+  console.log("COUPON", coupon);
+
+  const validCoupon = await Coupon.findOne({ name: coupon }).exec();
+  if (validCoupon === null) {
+    return res.json({
+      err: "Invalid coupon",
+    });
+  }
+  console.log("VALID COUPON", validCoupon);
+
+  const user = await User.findOne({ email: req.user.email }).exec();
+
+  let { products, cartTotal } = await Cart.findOne({ orderedBy: user._id })
+    .populate("products.product", "_id title price")
+    .exec();
+
+  console.log("cartTotal", cartTotal, "discount%", validCoupon.discount);
+
+  // calculate the total after discount
+  let totalAfterDiscount = (
+    cartTotal -
+    (cartTotal * validCoupon.discount) / 100
+  ).toFixed(2); // 99.99
+
+  console.log("----------> ", totalAfterDiscount);
+
+  Cart.findOneAndUpdate(
+    { orderedBy: user._id },
+    { totalAfterDiscount },
+    { new: true }
+  ).exec();
+
+  res.json(totalAfterDiscount);
 };
