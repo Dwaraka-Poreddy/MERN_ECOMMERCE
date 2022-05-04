b3cdc12a0da16132b41c9771d9c58f5ebed98f1d
Dwaraka-Poreddy <dwarakaporeddy124@gmail.com>
2022-05-04 19:43:41 +0530

    22 sections done. Users Previous orders page, invoice PDF download done

--- routes/user.js
+++ routes/user.js
@@ -11,12 +11,17 @@ const {
   emptyCart,
   saveAddress,
   applyCouponToUserCart,
+  createOrder,
+  orders,
 } = require("../controllers/user");
 
 router.post("/user/cart", authCheck, userCart); // save cart
 router.get("/user/cart", authCheck, getUserCart); // get cart
 router.delete("/user/cart", authCheck, emptyCart);
 router.post("/user/address", authCheck, saveAddress);
+router.post("/user/order", authCheck, createOrder);
+router.get("/user/orders", authCheck, orders);
+
 router.post("/user/cart/coupon", authCheck, applyCouponToUserCart);
 
 // router.get("/user", (req, res) => {
