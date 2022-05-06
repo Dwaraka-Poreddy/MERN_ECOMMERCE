b482ef1680ab2f07ecc95d9e0f079e3fa7d7d7e6
Dwaraka-Poreddy <dwarakaporeddy124@gmail.com>
2022-05-06 15:28:04 +0530

    debugging backend deployment-1

--- server.js
+++ server.js
@@ -23,4 +23,4 @@ readdirSync("./routes").map((r) => app.use("/api", require("./routes/" + r)));
 // port
 const port = process.env.PORT || 8000;
 
-app.listen(port, () => console.log(`Server is running on port ${port}`));
+app.listen(port, () => console.log(`Server is running on port number ${port}`));
