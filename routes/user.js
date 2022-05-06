8f268470b7806be5903e65114e1ee6155fde0fbc
Dwaraka-Poreddy <dwarakaporeddy124@gmail.com>
2022-05-06 12:04:09 +0530

    23, 24, 25 sections done. Admin dashboard (order management, wishlist, cash on delivery (cashless order) done. Deployment to be done.

--- routes/user.js
+++ routes/user.js
@@ -13,16 +13,26 @@ const {
   applyCouponToUserCart,
   createOrder,
   orders,
+  addToWishlist,
+  wishlist,
+  removeFromWishlist,
+  createCashOrder,
 } = require("../controllers/user");
 
 router.post("/user/cart", authCheck, userCart); // save cart
 router.get("/user/cart", authCheck, getUserCart); // get cart
 router.delete("/user/cart", authCheck, emptyCart);
 router.post("/user/address", authCheck, saveAddress);
-router.post("/user/order", authCheck, createOrder);
+
+router.post("/user/order", authCheck, createOrder); // stripe
+router.post("/user/cash-order", authCheck, createCashOrder); // cod
 router.get("/user/orders", authCheck, orders);
 
 router.post("/user/cart/coupon", authCheck, applyCouponToUserCart);
+
+router.post("/user/wishlist", authCheck, addToWishlist);
+router.get("/user/wishlist", authCheck, wishlist);
+router.put("/user/wishlist/:productId", authCheck, removeFromWishlist);
 
 // router.get("/user", (req, res) => {
 //   res.json({
